740834a75da8090bb4cfdb50ac06472ec57452c8
Rahul Amarwal <rahulamarwal418@gmail.com>
2026-06-06 22:03:48 +0530

    Fix: Increase password hash length for Postgres compatibility

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     orders = db.relationship('Order', backref='user', lazy=True)
 
